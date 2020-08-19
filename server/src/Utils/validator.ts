065d5efc2a0f09e8b37f37c49ba7902e9f58bc6b
nando <eunadno@gmail.com>
2020-08-19 20:54:05 -0300

    ajuste

---
+++ server/src/Utils/validator.ts
@@ -0,0 +1,76 @@
+import { Request, Response, NextFunction } from "express";
+import { Joi } from "celebrate";
+
+const rules = {
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z-0-9]{3,30}$")),
+};
+const options = { abortEarly: true };
+
+const accountSingIn = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  const schema = Joi.object({
+    email: rules.email,
+    password: rules.password,
+  });
+  const { error } = schema.validate({ email, password }, options);
+
+  if (error) {
+    const details = error["details"][0];
+    const { type, path } = details;
+    
+    return res.json({
+      type,
+      fieldName: path[0],
+    });
+  };
+
+  next();
+};
+
+const accountSingUp = (req: Request, res: Response, next: NextFunction) => {
+  const {
+    email,
+    password,
+    password_confirmation,
+    name,
+    avatar,
+    whatsapp,
+    bio,
+  } = req.body;
+
+  const schema = Joi.object({
+    email: rules.email,
+    password: rules.password,
+    password_confirmation: Joi.string().valid(Joi.ref('password')).required(),
+    name: Joi.string().required(),
+    avatar: Joi.string().required(),
+    bio: Joi.string(),
+    whatsapp: Joi.string().pattern(
+      new RegExp(/^([0-9]{2})([9]{1})?([0-9]{4})([0-9]{4})$/)
+    ),
+  });
+
+  const { error } = schema.validate({ 
+    email, password, password_confirmation, name, avatar, whatsapp, bio 
+  }, options);
+
+  if (error) {
+    const details = error["details"][0];
+    const { type, path } = details;
+    
+    return res.json({
+      type,
+      fieldName: path[0],
+    });
+  }
+
+  next();
+};
+
+
+export {
+  accountSingIn,
+  accountSingUp,
+}
